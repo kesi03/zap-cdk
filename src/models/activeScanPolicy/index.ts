b12e2e26e1dc0415f27af3a1ede19acbb1a747fb
Kester Simm <68278724+kesi03@users.noreply.github.com>
2025-09-03 09:42:21 +0000

    fixed sonar bug

--- src/models/activeScanPolicy/index.ts
+++ src/models/activeScanPolicy/index.ts
@@ -1,22 +1,25 @@
+export type strength = 'Low' | 'Medium' | 'High' | 'Insane';
+export type threshold = 'Off' | 'Low' | 'Medium' | 'High';
+
 export interface IAlertTag {
   include?: string[]; // List of alert tags to include, regex supported
   exclude?: string[]; // List of alert tags to exclude from this include list, regex supported
-  strength?: 'Low' | 'Medium' | 'High' | 'Insane'; // The Attack Strength for this set of rules, default: Medium
-  threshold?: 'Off' | 'Low' | 'Medium' | 'High'; // The Alert Threshold for this set of rules, default: Medium
+  strength?: strength; // The Attack Strength for this set of rules, default: Medium
+  threshold?: threshold; // The Alert Threshold for this set of rules, default: Medium
 }
 
 export interface IRule {
   id: number; // The rule id as per https://www.zaproxy.org/docs/alerts/
   name?: string; // The name of the rule for documentation purposes - this is not required or actually used
-  strength?: 'Low' | 'Medium' | 'High' | 'Insane'; // The Attack Strength for this rule, default: Medium
-  threshold?: 'Off' | 'Low' | 'Medium' | 'High'; // The Alert Threshold for this rule, default: Medium
+  strength?: strength; // The Attack Strength for this rule, default: Medium
+  threshold?: threshold; // The Alert Threshold for this rule, default: Medium
 }
 
 export interface IActiveScanPolicyParameters {
   name: string; // Name of the policy, mandatory
   policyDefinition: {
-    defaultStrength?: 'Low' | 'Medium' | 'High' | 'Insane'; // Default Attack Strength for all rules, default: Medium
-    defaultThreshold?: 'Off' | 'Low' | 'Medium' | 'High'; // Default Alert Threshold for all rules, default: Medium
+    defaultStrength?: strength; // Default Attack Strength for all rules, default: Medium
+    defaultThreshold?: threshold; // Default Alert Threshold for all rules, default: Medium
     alertTags?: IAlertTag; // Rules based on alert tags
     rules?: IRule[]; // A list of one or more active scan rules
   };
